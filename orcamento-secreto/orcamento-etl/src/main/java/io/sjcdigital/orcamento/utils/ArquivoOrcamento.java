0da2debf4a243afc4d0e677b20dac7f3e88c5cd1
Pedro Hos <pedro-hos@outlook.com>
2022-10-24 18:43:28 -0300

    .

--- orcamento-secreto/orcamento-etl/src/main/java/io/sjcdigital/orcamento/utils/ArquivoOrcamento.java
+++ orcamento-secreto/orcamento-etl/src/main/java/io/sjcdigital/orcamento/utils/ArquivoOrcamento.java
@@ -19,30 +19,30 @@ import io.sjcdigital.orcamento.model.pojo.ArquivosPojo;
 public class ArquivoOrcamento {
 
     private static final String PATH = "/home/pesilva/workspace/code/pessoal/data-analizes/orcamento-secreto/data-json/";
-    
+
     public static void main(String[] args) {
-        
+
         List<String> anos = Arrays.asList("2020", "2021", "2022");
         List<ArquivosPojo> arquivosPojo = new ArrayList<>();
         ObjectMapper mapper = new ObjectMapper();
-        
+
         anos.forEach(ano -> {
 
             System.out.println("Coletando " + ano + "\n");
-            
+
             Path path = Paths.get(PATH + ano);
-            
+
             try {
 
                 listFiles(path).forEach(arquivo -> {
 
-
                     try {
-                        
+
                         Emendas emenda = mapper.readValue(arquivo.toFile(), Emendas.class);
                         String nomeArquivo = "/" + emenda.ano + "/" + arquivo.getFileName().toString();
-                        arquivosPojo.add(new ArquivosPojo(emenda.codigoEmenda, emenda.funcao, emenda.subfuncao, nomeArquivo));
-                        
+                        arquivosPojo.add(
+                                new ArquivosPojo(emenda.codigoEmenda, emenda.funcao, emenda.subfuncao, nomeArquivo));
+
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -54,34 +54,34 @@ public class ArquivoOrcamento {
             }
 
         });
-        
+
         System.out.println(arquivosPojo.size());
-        
+
         salvaJSON(arquivosPojo);
     }
-    
+
     private static void salvaJSON(List<ArquivosPojo> arquivosPojo) {
         String fileName = "arquivos";
         createJsonFile(PATH, fileName, arquivosPojo);
     }
-    
+
     private static void createJsonFile(String directoryName, String arquivoNome, List<ArquivosPojo> arquivosPojo) {
-        
+
         try {
-            Files.write(Paths.get(directoryName + arquivoNome + ".json"), new ObjectMapper().writeValueAsString(arquivosPojo).getBytes());
+            Files.write(Paths.get(directoryName + arquivoNome + ".json"),
+                    new ObjectMapper().writeValueAsString(arquivosPojo).getBytes());
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
+
     public static List<Path> listFiles(Path path) throws IOException {
 
         List<Path> result;
         try (Stream<Path> walk = Files.walk(path)) {
-            result = walk.filter(Files::isRegularFile)
-                    .collect(Collectors.toList());
+            result = walk.filter(Files::isRegularFile).collect(Collectors.toList());
         }
         return result;
 
